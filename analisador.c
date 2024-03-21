403709e1978860c9b85f00976ba6d986a09cf7a0
Leonardo Simon Monteiro <135035590+LeoSimonn@users.noreply.github.com>
2024-03-21 23:09:41 +0000

    Fix formatting and spacing in analisador.c

--- analisador.c
+++ analisador.c
@@ -7,7 +7,7 @@ expressões aritméticas simples */
 /* Declarações globais */
 /* Variáveis */
 int charClass;
-char lexeme [100];
+char lexeme[100];
 char nextChar;
 int lexLen;
 int token;
@@ -39,13 +39,16 @@ int lex();
 
 /**********************************************/
 /* função principal */
-main() {
+main()
+{
     /* Abre o arquivo de dados de entrada e processa seu conteúdo */
     if ((in_fp = fopen("front.in", "r")) == NULL)
         printf("ERROR - cannot open front.in \n");
-    else {
+    else
+    {
         getChar();
-        do {
+        do
+        {
             lex();
         } while (nextToken != EOF);
     }
@@ -53,44 +56,48 @@ main() {
 /******************************************************/
 /* lookup - uma função para processar operadores e parênteses
    e retornar o token */
-int lookup(char ch) {
-    switch (ch) {
-        case '(':
-            addChar();
-            nextToken = LEFT_PAREN;
-            break;
-        case ')':
-            addChar();
-            nextToken = RIGHT_PAREN;
-            break;
-        case '+':
-            addChar();
-            nextToken = ADD_OP;
-            break;
-        case '-':
-            addChar();
-            nextToken = SUB_OP;
-            break;
-        case '*':
-            addChar();
-            nextToken = MULT_OP;
-            break;
-        case '/':
-            addChar();
-            nextToken = DIV_OP;
-            break;
-        default:
-            addChar();
-            nextToken = EOF;
-            break;
+int lookup(char ch)
+{
+    switch (ch)
+    {
+    case '(':
+        addChar();
+        nextToken = LEFT_PAREN;
+        break;
+    case ')':
+        addChar();
+        nextToken = RIGHT_PAREN;
+        break;
+    case '+':
+        addChar();
+        nextToken = ADD_OP;
+        break;
+    case '-':
+        addChar();
+        nextToken = SUB_OP;
+        break;
+    case '*':
+        addChar();
+        nextToken = MULT_OP;
+        break;
+    case '/':
+        addChar();
+        nextToken = DIV_OP;
+        break;
+    default:
+        addChar();
+        nextToken = EOF;
+        break;
     }
     return nextToken;
 }
 
 /******************************************************/
 /* addChar - uma função para adicionar nextChar a lexeme */
-void addChar() {
-    if (lexLen <= 98) {
+void addChar()
+{
+    if (lexLen <= 98)
+    {
         lexeme[lexLen++] = nextChar;
         lexeme[lexLen] = 0;
     }
@@ -101,13 +108,16 @@ void addChar() {
 /******************************************************/
 /* getChar - uma função para obter o próximo caractere de
    entrada e determinar sua classe */
-void getChar() {
-    if ((nextChar = getc(in_fp)) != EOF) {
+void getChar()
+{
+    if ((nextChar = getc(in_fp)) != EOF)
+    {
         if (isalpha(nextChar))
             charClass = LETTER;
         else if (isdigit(nextChar))
             charClass = DIGIT;
-        else charClass = UNKNOWN;
+        else
+            charClass = UNKNOWN;
     }
     else
         charClass = EOF;
@@ -116,53 +126,58 @@ void getChar() {
 /******************************************************/
 /* getNonBlank - uma função para chamar getChar até que ele
    retorne um caractere que não seja um espaço em branco */
-void getNonBlank() {
+void getNonBlank()
+{
     while (isspace(nextChar))
         getChar();
 }
 
 /******************************************************/
 /* lex - um analisador léxico simples para expressões aritméticas */
-int lex() {
+int lex()
+{
     lexLen = 0;
     getNonBlank();
-    switch (charClass) {
-        /* Análise identificadores sintaticamente */
-        case LETTER:
+    switch (charClass)
+    {
+    /* Análise identificadores sintaticamente */
+    case LETTER:
+        addChar();
+        getChar();
+        while (charClass == LETTER || charClass == DIGIT)
+        {
             addChar();
             getChar();
-            while (charClass == LETTER || charClass == DIGIT) {
-                addChar();
-                getChar();
-            }
-            nextToken = IDENT;
-            break;
-        
-        /* Análise inteiros literais */
-        case DIGIT:
+        }
+        nextToken = IDENT;
+        break;
+
+    /* Análise inteiros literais */
+    case DIGIT:
+        addChar();
+        getChar();
+        while (charClass == DIGIT)
+        {
             addChar();
             getChar();
-            while (charClass == DIGIT) {
-                addChar();
-                getChar();
-            }
-            nextToken = INT_LIT;
-            break;
-        
-        /* Parênteses e operadores */
-        case UNKNOWN:
-            lookup(nextChar);
-            getChar();
-            break;
-        
-        /* Fim do arquivo */
-        case EOF:
-            nextToken = EOF;
-            lexeme[0] = 'E';
-            lexeme[1] = 'O';
-            lexeme[2] = 'F';
-            lexeme[3] = 0;
-            break;
+        }
+        nextToken = INT_LIT;
+        break;
+
+    /* Parênteses e operadores */
+    case UNKNOWN:
+        lookup(nextChar);
+        getChar();
+        break;
+
+    /* Fim do arquivo */
+    case EOF:
+        nextToken = EOF;
+        lexeme[0] = 'E';
+        lexeme[1] = 'O';
+        lexeme[2] = 'F';
+        lexeme[3] = 0;
+        break;
     } /* Fim do switch */
     printf("Next token is: %d, Next lexeme is %s\n",
            nextToken, lexeme);
