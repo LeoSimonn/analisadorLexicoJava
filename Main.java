d1d97541db7874d909c636b76571278744931b2c
Arthur Henz <arthur.henz@edu.pucrs.br>
2024-03-30 17:21:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("front.in"));
